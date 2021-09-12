0f8662c7ba06ddd44bedb30f7b7936596679d6f2
KuziaMak <kuz-mak9@yandex.ru>
2021-09-12 18:28:04 +0300

    middle

--- 3 выполните практические задания/3 выполните практические задания/обязательная часть/app.js
+++ 3 выполните практические задания/3 выполните практические задания/обязательная часть/app.js
@@ -32,5 +32,37 @@ filterSizeWrap.addEventListener('click', function () {
 let buyWeb = document.querySelector(".cartIconWrap")
 let buy = 0
 buyWeb.childNodes[3].innerText = buy
+let summ = 0
+let mass = {}
+let content = "" // display: none;
+buyWeb.insertAdjacentHTML("afterend", `
+<table class="basket"  border="1px" style="background: white; "> 
+<tr>
+ <th>Название товара</th>
+ <th>Количество</th>
+ <th>Цена</th>
+ <th>Итого</th>
+</tr>
+${content}
+<tr>
+ <td colspan="4" style="text-align: right;">Товар в корзине на сумму: ${summ}</td>
+</tr>
+</table> `)
 
-buyWeb.insertAdjacentHTML("afterend", `<br>название товара <br>`)
+let featuredImgAll = document.querySelectorAll(".featuredImgDark > button")
+featuredImgAll.forEach((even) => {
+    even.addEventListener("click", (elem) => {
+        if (mass[elem.path[3].childNodes[3].firstElementChild.innerText] === undefined) {
+            mass[elem.path[3].childNodes[3].firstElementChild.innerText] = {
+                quantity: 1,
+                price: +elem.path[3].childNodes[3].lastElementChild.innerText.substring(1),
+                dop: elem.path[3].childNodes[3].lastElementChild.innerText.substring(1)
+            }
+            console.log((mass[elem.path[3].childNodes[3].firstElementChild.innerText]))
+
+        }
+        console.log(mass)
+
+    })
+}) //elem.path[3].childNodes[3].firstElementChild.innerText
+// +elem.path[3].childNodes[3].lastElementChild.innerText.substring(1)
